60b156a7a463fea75bde9d4f7fe76d9496c8b9cd
Erick <erickvillaseor@yahoo.com.mx>
2025-09-27 09:56:58 -0600

    Se le agrego un icono a la pwa

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.meq1aicqh3"
+    "revision": "0.i85mb9q8un"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
